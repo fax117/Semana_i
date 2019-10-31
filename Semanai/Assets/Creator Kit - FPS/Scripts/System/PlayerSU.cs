6ed55d83b58a7ff6fc14316f05bd927c29343bc0
fax117 <38862497+fax117@users.noreply.github.com>
2019-10-30 23:27:54 -0600

    MP 0.6
    mejoras

---
+++ Semanai/Assets/Creator Kit - FPS/Scripts/System/PlayerSU.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSU : NetworkBehaviour {
+    public Behaviour[] componentsToDisable;
+    Camera sceneCamera;
+    void Start()
+    {
+        if(!isLocalPlayer){
+            for(int i = 0; i < componentsToDisable.Length; i++)
+                componentsToDisable[i].enabled = false; 
+        }
+        else{
+            sceneCamera = Camera.main;
+
+            if(sceneCamera != null){
+                sceneCamera.gameObject.SetActive(false);
+            } 
+        }
+               
+    }
+
+    private void OnDisable() {
+        if(sceneCamera != null){
+            sceneCamera.gameObject.SetActive(true);
+        }     
+    }
+
+    
+}
---
+++ Semanai/Assets/PlayerSetup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+    Behaviour[] componentsToDisable;
+    void Start()
+    {
+        if(!isLocalPlayer){
+            for(int i = 0; i < componentsToDisable.Length; i++)
+                componentsToDisable[i].enabled = false; 
+        }       
+    }
+}
